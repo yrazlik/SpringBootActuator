8d9913807e170c759fb372dfb40d72ec6c69a418
Yasin Razlik <yrazlik@gmail.com>
2020-05-15 00:03:09 +0300

    Removed custome health indicator so that we can use the default health indicator

--- src/main/java/guru/springframework/actuator/health/CustomHealthIndicator.java
+++ src/main/java/guru/springframework/actuator/health/CustomHealthIndicator.java
@@ -6,7 +6,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class CustomHealthIndicator implements HealthIndicator{
 
 	private Random random = new Random();
